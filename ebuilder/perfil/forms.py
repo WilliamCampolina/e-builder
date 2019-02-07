341d79ff97f57622eda48f5f0c0b282d4b28104b
william-casa <william.campolina@gmail.com>
2019-02-07 11:41:03 -0200

    alteracao tipo do form

--- ebuilder/perfil/forms.py
+++ ebuilder/perfil/forms.py
@@ -7,8 +7,14 @@ class PerfilEmpresaForm(forms.ModelForm):
     class Meta:
         model = PerfilEmpresa
         fields = ['topicos', 'razaoSocial', 'nome', 'telefone', 'email', 'rua', 'cep', 'cidade', 'estado', 'pais']
+        widgets = {
+            'topicos': forms.SelectMultiple(attrs={'size': '12'}),
+        }
 
 class PerfilProfissionalForm(forms.ModelForm):
     class Meta:
         model = PerfilProfissional
         fields = ['servicos', 'nome', 'sobrenome', 'telefone', 'email', 'rua', 'cep', 'cidade', 'estado', 'pais']
+        widgets = {
+            'servicos': forms.SelectMultiple(attrs={'size': '12'}),
+        }
